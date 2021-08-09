d2434c672546ff160c7a353974ee199b18a7b86b
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-09 14:02:37 -0400

    lecture work-in-progress

--- followAlong/src/reducers/index.js
+++ followAlong/src/reducers/index.js
@@ -10,19 +10,27 @@ const reducer = (state, action) => {
     switch(action.type) {
         case(TOGGLE_EDIT):
             return({ ...state, editing: !state.editing });
+        case(SET_TITLE_TEXT):
+            return({ ...state, newTitleText: action.payload });
+        case(SAVE_INPUT):
+            return({
+                ...state,
+                editing:false,
+                title: state.newTitleText
+            });
     }
 
-    if(action.type === TOGGLE_EDIT) {
-        return({ ...state, editing: !state.editing });
-    } else if (action.type === SET_TITLE_TEXT) {
-        return({ ...state, newTitleText: action.payload });
-    } else if (action.type === SAVE_INPUT) {
-        return({
-            ...state,
-            editing:false,
-            title: state.newTitleText
-        });
-    }
+    // if(action.type === TOGGLE_EDIT) {
+    //     return({ ...state, editing: !state.editing });
+    // } else if (action.type === SET_TITLE_TEXT) {
+    //     return({ ...state, newTitleText: action.payload });
+    // } else if (action.type === SAVE_INPUT) {
+    //     return({
+    //         ...state,
+    //         editing:false,
+    //         title: state.newTitleText
+    //     });
+    // }
 
     return state;
 }
