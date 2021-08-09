c2e1b51b57d7a47b77934320441bdf136713ffba
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-09 13:40:55 -0400

    lecture work-in-progress

--- followAlong/src/components/Title.js
+++ followAlong/src/components/Title.js
@@ -1,9 +1,11 @@
 import React, { useState, useReducer } from 'react';
+import reducer, { initialState} from './../reducers'
 
 const Title = () => {
-  const [title, setTitle] = useState('Hello earthlings!');
-  const [editing, setEditing] = useState(false);
-  const [newTitleText, setNewTitleText] = useState('');
+  // const [title, setTitle] = useState('Hello earthlings!');
+  // const [editing, setEditing] = useState(false);
+  // const [newTitleText, setNewTitleText] = useState('');
+  const [state, dispatch ] = useReducer();
 
   const handleChanges = e => {
     setNewTitleText(e.target.value);
