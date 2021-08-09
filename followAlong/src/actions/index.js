7e8e468ce0a0de6149a834b96a534975433dca2c
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-09 13:52:44 -0400

    lecture work-in-progress

--- followAlong/src/actions/index.js
+++ followAlong/src/actions/index.js
@@ -1,5 +1,10 @@
 export const TOGGLE_EDIT = "TOGGLE_EDIT";
+export const SET_TITLE_TEXT = "SET_TITLE_TEXT";
 
 export const toggleEdit = ()=> {
     return({type:"TOGGLE_EDIT"})
+}
+
+export const setTitleText = ()=> {
+    return({type:"SET_TITLE_TEXT"})
 }
